2b10dec6d444d0a18faae6b1bd395796a75b312e
Stefan <37225048+StefanDjokovic@users.noreply.github.com>
2019-04-11 23:46:18 +0200

    A performance update, now 2.5X faster
    Some results compared to the old code:
    N9, 270 superpermutations: from 73s down to 32s
    N6, 1024 superpermutations: from 457ms down to 180ms
    N5, 36 superperpermutations: kinda trivial to calculate, but on the trials the new version always took half the time

--- SupPerm_Checker-from-file.c
+++ SupPerm_Checker-from-file.c
@@ -10,10 +10,10 @@
 int factorial(int val);
 
 //function that gets a specific hashvalue of a string of N elements, returns -1 if invalid permutation, more info in README
-long int getHashValue(char * string, int N);
+long int getHashValue(int * string, int N, int * factval, bool * arr);
 
 //the checker function
-int runChecker(int * str, bool * arr, int to, int N);
+int runChecker(int * str, bool * arr, int to, int N, int * factval, int * perm);
 
 
 //The main, only for permutation of numbers from 1
@@ -35,9 +35,14 @@ int main() {
 		return -1;
 	}
 
+	int * factval = (int*)malloc((N + 1) * sizeof(int)); //contains factorial values
+	for (int i = 0; i <= N; i++) {
+		factval[i] = factorial(i);
+	}
+
 	//code that ignores # lines
 	char c;
-	c = fgetc(fp);	
+	c = fgetc(fp);
 	while (c == '#') {
 		while (fgetc(fp) != '\n') {
 			//do nothing til we get to new line
@@ -51,10 +56,10 @@ int main() {
 	while (b != '\n') {
 		b = fgetc(fp);
 		size++;
-	}	
+	}
 	rewind(fp);
 
-	//re-doing because I rewinded the file pointer
+	//re-ignoring # lines because I rewinded the file pointer
 	c = fgetc(fp);
 	while (c == '#') {
 		while (fgetc(fp) != '\n') { //go till new line
@@ -62,27 +67,35 @@ int main() {
 		c = fgetc(fp);
 	}
 
+	int *perm = (int*)malloc(sizeof(int)*(N + 1));
+	bool *checker = (bool*)malloc(factval[N] * sizeof(bool));
 	for (int j = 1; c != EOF; j++) {
-		bool *checker = (bool*)calloc(factorial(N), sizeof(bool));
+		for (int i = 0; i < factval[N]; i++)
+			checker[i] = 0;
+		//bool *checker = (bool*)calloc(factval[N], sizeof(bool));
 
 		int * stringToCheck = (int *)malloc((size) * sizeof(int));
 		stringToCheck[0] = c - '1';
 		for (int i = 1; i < size; i++) {
 			stringToCheck[i] = fgetc(fp) - '1'; //if in letters change 1 into A
 		}
+		/*for (int i = 0; i < size; i++) {
+			printf("%d", stringToCheck[i]);
+		}
+		printf("\n");*/
 
-		int test = runChecker(stringToCheck, checker, size, N);
+		int test = runChecker(stringToCheck, checker, size, N, factval, perm);
 
-		if (test == 1)
+		/*if (test == 1)
 			printf("Perm %d : Yes, it's a superpermutation\n", j);
 		else
-			printf("Perm %d : Not a superpermutation\n", j);
+			printf("Perm %d : Not a superpermutation\n", j);*/
 
 		free(stringToCheck);
 
-		free(checker);
+		//free(checker);
 
-		c = fgetc(fp);
+		c = fgetc(fp); //clearing from /n
 		c = fgetc(fp);
 	}
 
@@ -93,47 +106,44 @@ int main() {
 
 
 
-int runChecker(int * str, bool * arr, int to, int N) {
-	char *perm = (char*)malloc(sizeof(char)*(N + 1));
-	perm[N] = '\0';
+int runChecker(int * str, bool * arr, int to, int N, int * factval, int * perm) {
 	long int hashValue;
+
+	bool *hashvaluearr = (bool*)calloc(N, sizeof(bool));
 
 	for (int j = 0; j <= to - N; j++) {
 		for (int i = 0; i < N; i++) {
 			perm[i] = str[j + i];
-			//printf("%d", perm[i]);
 		}
-		//printf(" %d\n", j);
-		hashValue = getHashValue(perm, N);
+		hashValue = getHashValue(perm, N, factval, hashvaluearr);
 		if (hashValue != -1)
 			arr[hashValue] = 1;
-		//printf("wow %d ", j);
+
+		for (int i = 0; i < N; i++)
+			hashvaluearr[i] = 0;
 	}
 
-	for (int i = 0; i < factorial(N); i++) {
+	for (int i = 0; i < factval[N]; i++) {
 		if (arr[i] != 1) {
 			return 0;
 		}
 
 	}
 
-	free(perm);
-
 	return 1;
 }
 
-long int getHashValue(char * string, int N) {
+//how it works in README
+long int getHashValue(int * string, int N, int * factval, bool * arr) {
 	long int val = 0;
-	bool *arr = (bool*)calloc(N, sizeof(bool));
 
-	val = (string[0]) * factorial(N - 1);
+	val = (string[0]) * factval[N - 1];
 	arr[string[0]] = 1;
 
-	for (int p = 1; p < N; p++) {
+	for (int p = 1; p < N; p++) { 
 		int k = 0;
 		int count = 0;
 		int step = string[p];
-		//printf(" step : %d ", string[p]);
 		while (k != step) {
 			if (arr[k] != 1)
 				count++;
@@ -144,11 +154,9 @@ long int getHashValue(char * string, int N) {
 		else
 			return -1;
 
-		val = val + count * factorial(N - (p + 1));
+		val = val + count * factval[N - (p + 1)];	
 	}
 
-	free(arr);
-	//printf(" val: %d ", val);
 	return val;
 }
 
